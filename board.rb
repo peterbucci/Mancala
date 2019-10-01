260b36d3f7239d8262f148ca29623aa0240fb564
Peter <sb1285n@gmail.com>
2019-10-01 14:22:44 -0400

    in Game.traverse_board added an if statement to check to see if the player ends on a pit with seeds or their own seed bank.

--- board.rb
+++ board.rb
@@ -10,15 +10,27 @@ class Board
     in_hand = @pits[@current_pit]
     @pits[@current_pit] = 0
 
+    # Consider removing this eventually.
+    puts "You picked up #{in_hand} seeds from pit #{@current_pit}."
+    sleep(1)
+
     in_hand
   end
 
   def sow_seed
     @pits[@current_pit] += 1
+
+    # Consider removing this eventually.
+    puts "You planted a single seed in pit #{@current_pit}. Current Board: #{@pits}"
+    sleep(1)
   end
 
   def traverse
     @current_pit += 1
+
+      # Consider removing this eventually.
+      puts "You moved to pit #{@current_pit}."
+      sleep(1)
   end
 
   def empty?
--- game.rb
+++ game.rb
@@ -15,7 +15,7 @@ class Game
     until win?
       players_board = current_player[1]
 
-      p current_player
+      puts "Pick a number between 0 and 5."
       chosen_pit = gets.chomp.to_i
 
       players_board.select_pit(chosen_pit)
@@ -30,6 +30,10 @@ class Game
 
   def traverse_board(seeds_in_hand, current_player, current_side)
     until seeds_in_hand == 0
+      # Consider removing this eventually.
+      puts "You have #{seeds_in_hand} seed(s) left."
+      sleep(1)
+
       if current_side[1].current_pit == 5
         current_player[0].bank_seed if current_player == current_side
           
@@ -39,8 +43,17 @@ class Game
         current_side[1].traverse
         current_side[1].sow_seed
       end
-      
+
       seeds_in_hand -= 1 if current_side[1].current_pit != 5 || current_player == current_side
+    end
+
+    # Consider removing this eventually.
+    puts "You're now out of seeds"
+    sleep(1)
+
+    if current_side[1].current_pit > 0 || (current_player != current_side && current_side[1].current_pit == -1)
+      puts "You ended your turn on a pit with seeds or your seed bank. You can keep going."
+      sleep(1)
     end
   end
 
